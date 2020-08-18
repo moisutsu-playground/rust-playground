9d4b3fd9832db06aa0889c878c5a776723a5334b
moisutsu <moisutsu@gmail.com>
2020-08-18 23:59:42 +0900

    Use xml-rs

--- arxiv-api-sample/src/main.rs
+++ arxiv-api-sample/src/main.rs
@@ -1,4 +1,5 @@
 use anyhow::{anyhow, Result};
+use xml::reader::{EventReader, XmlEvent};
 
 #[async_std::main]
 async fn main() -> Result<()> {
@@ -6,6 +7,38 @@ async fn main() -> Result<()> {
     let url = format!("{}{}", base_url, "search_query=cat:cs.CL");
     let mut response = surf::get(url).await.map_err(|err| anyhow!(err))?;
     let body = response.body_string().await.map_err(|err| anyhow!(err))?;
-    println!("{}", body);
+
+    let mut summarys = Vec::new();
+
+    let parser = EventReader::from_str(&body);
+    let mut depth = 0;
+    for e in parser {
+        match e {
+            Ok(XmlEvent::StartElement { name, .. }) => {
+                println!("{}+{}", indent(depth), name);
+                depth += 1;
+            }
+            Ok(XmlEvent::Characters(data)) => {
+                println!("{}+{}", indent(depth), data);
+                summarys.push(data);
+            }
+            Ok(XmlEvent::EndElement { name }) => {
+                depth -= 1;
+                println!("{}-{}", indent(depth), name);
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+                break;
+            }
+            _ => {}
+        }
+    }
     Ok(())
+}
+
+fn indent(size: usize) -> String {
+    const INDENT: &'static str = "    ";
+    (0..size)
+        .map(|_| INDENT)
+        .fold(String::with_capacity(size * INDENT.len()), |r, s| r + s)
 }
