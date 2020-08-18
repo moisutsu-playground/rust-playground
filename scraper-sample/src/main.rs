f15a78def6b1bb971adb3994df3e5d7cbb7c6d9e
moisutsu <moisutsu@gmail.com>
2020-08-18 14:36:09 +0900

    Add get html body sample

--- scraper-sample/src/main.rs
+++ scraper-sample/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+#[async_std::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
+    let url = "https://www.google.com/";
+    println!("{}", get_html_body(url).await?);
+    Ok(())
+}
+
+async fn get_html_body(url: &str) -> Result<String, Box<dyn std::error::Error + Send + Sync + 'static>> {
+    let mut response = surf::get(url).await?;
+    let body = response.body_string().await?;
+    Ok(body)
 }
