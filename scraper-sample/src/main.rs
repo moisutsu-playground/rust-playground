bbcf7f87cdc9a6b120f4e8b32372a156902ddfbc
moisutsu <moisutsu@gmail.com>
2020-08-18 18:16:16 +0900

    Add scrapping ACL2020 titles sample

--- scraper-sample/src/main.rs
+++ scraper-sample/src/main.rs
@@ -1,7 +1,14 @@
+use scraper::{Html, Selector};
+
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
-    let url = "https://www.google.com/";
-    println!("{}", get_html_body(url).await?);
+    let url = "https://www.aclweb.org/anthology/events/acl-2020/";
+    let body = get_html_body(url).await?;
+    let fragment = Html::parse_fragment(&body);
+    let selector = Selector::parse("#\\32 020-acl-main > p > span:nth-child(2) > strong > a").unwrap();
+    for element in fragment.select(&selector) {
+        println!("{:?}", element.inner_html());
+    }
     Ok(())
 }
 
