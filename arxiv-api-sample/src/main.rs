2b736b63e3869d73f0a4181e2f05a38143c68dd6
moisutsu <moisutsu@gmail.com>
2020-08-19 20:49:39 +0900

    Add sample to store data in a structure

--- arxiv-api-sample/src/main.rs
+++ arxiv-api-sample/src/main.rs
@@ -1,6 +1,23 @@
 use anyhow::{anyhow, Result};
 use xml::reader::{EventReader, XmlEvent};
 
+#[derive(Debug, Clone)]
+struct Arxiv {
+    id: String,
+    title: String,
+    summary: String,
+}
+
+impl Arxiv {
+    fn new() -> Self {
+        Arxiv {
+            id: "".to_string(),
+            title: "".to_string(),
+            summary: "".to_string(),
+        }
+    }
+}
+
 #[async_std::main]
 async fn main() -> Result<()> {
     let base_url = "http://export.arxiv.org/api/query?";
@@ -8,37 +25,59 @@ async fn main() -> Result<()> {
     let mut response = surf::get(url).await.map_err(|err| anyhow!(err))?;
     let body = response.body_string().await.map_err(|err| anyhow!(err))?;
 
-    let mut summarys = Vec::new();
+    println!("{}", body);
 
-    let parser = EventReader::from_str(&body);
-    let mut depth = 0;
-    for e in parser {
-        match e {
-            Ok(XmlEvent::StartElement { name, .. }) => {
-                println!("{}+{}", indent(depth), name);
-                depth += 1;
+    let mut parser = EventReader::from_str(&body);
+    let mut arxiv = Arxiv::new();
+    let mut arxivs = Vec::new();
+
+    'outer: loop {
+        if let Ok(element) = parser.next() {
+            match element {
+                XmlEvent::StartElement { name, .. } => {
+                    match &name.local_name[..] {
+                        "entry" => {
+                            arxiv = Arxiv::new();
+                        }
+                        "id" => {
+                            if let Ok(XmlEvent::Characters(id)) = parser.next() {
+                                arxiv.id = id;
+                            }
+                        }
+                        "title" => {
+                            if let Ok(XmlEvent::Characters(title)) = parser.next() {
+                                arxiv.title = title;
+                            }
+                        }
+                        "summary" => {
+                            if let Ok(XmlEvent::Characters(summary)) = parser.next() {
+                                arxiv.summary = summary;
+                            }
+                        }
+                        _ => ()
+                    }
+                }
+                XmlEvent::EndElement { name } => {
+                    match &name.local_name[..] {
+                        "entry" => {
+                            arxivs.push(arxiv.clone());
+                        }
+                        "feed" => {
+                            break 'outer;
+                        }
+                        _ => ()
+                    }
+                }
+                _ => {
+                    ()
+                }
             }
-            Ok(XmlEvent::Characters(data)) => {
-                println!("{}+{}", indent(depth), data);
-                summarys.push(data);
-            }
-            Ok(XmlEvent::EndElement { name }) => {
-                depth -= 1;
-                println!("{}-{}", indent(depth), name);
-            }
-            Err(e) => {
-                println!("Error: {}", e);
-                break;
-            }
-            _ => {}
+        } else {
+            break;
         }
     }
+    for i in 0..arxivs.len() {
+        println!("{:?}", arxivs[i]);
+    }
     Ok(())
-}
-
-fn indent(size: usize) -> String {
-    const INDENT: &'static str = "    ";
-    (0..size)
-        .map(|_| INDENT)
-        .fold(String::with_capacity(size * INDENT.len()), |r, s| r + s)
 }
