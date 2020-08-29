37c849ff18202f33c485c8e22b929723cf394fe0
moisutsu <moisutsu@gmail.com>
2020-08-29 19:36:58 +0900

    Add const fn sample

--- bins/src/bin/const_fn_sample.rs
+++ bins/src/bin/const_fn_sample.rs
@@ -1,30 +1,11 @@
-use proconio::input;
-
-const fn add(a: i32, b: i32) -> i32 {
+const fn cul(a: i32, b: i32) -> i32 {
     a + b
 }
 
-// const fn add(a: i32, b: i32) -> i32 {
-    // if a > 5 {
-        // a + b
-    // } else {
-        // b + a
-    // }
-// }
-
-use std::io::{stdout, Write};
-macro_rules! flush {
-    () => {
-        stdout().flush().unwrap();
-    };
-}
+const X: i32 = 10;
+const Y: i32 = 21;
 
 fn main() {
-    print!("Input: ");
-    flush!();
-    input! {
-        x: i32,
-        y: i32,
-    }
-    println!("{}", add(x, y));
+    const RESULT: i32 = cul(X, Y);
+    println!("{}", RESULT);
 }
