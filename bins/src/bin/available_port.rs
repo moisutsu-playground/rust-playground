49e6b65c26b60c345567961cc8de194375b5ec8e
moisutsu <moisutsu@gmail.com>
2020-09-13 21:55:30 +0900

    Cargo fmt for available_port.rs

--- bins/src/bin/available_port.rs
+++ bins/src/bin/available_port.rs
@@ -2,7 +2,9 @@ use std::io::Result;
 use std::net::TcpListener;
 
 fn available_port() -> Result<u16> {
-    TcpListener::bind("localhost:0")?.local_addr().map(|addr| addr.port())
+    TcpListener::bind("localhost:0")?
+        .local_addr()
+        .map(|addr| addr.port())
 }
 
 fn main() -> Result<()> {
