16d03eaa50b6950688f350e980a9a565bea9545c
moisutsu <moisutsu@gmail.com>
2020-11-13 22:38:32 +0900

    Add stream output

--- pipe-sample/src/main.rs
+++ pipe-sample/src/main.rs
@@ -1,3 +1,16 @@
+use std::process::{Command, Stdio};
+
 fn main() {
-    println!("Hello, world!");
+    let mut cmd = Command::new("fd")
+        .arg("python")
+        .arg("/")
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .spawn()
+        .unwrap();
+
+    // It's streaming here
+
+    cmd.wait().unwrap();
+    println!("Exited with status {:?}", status);
 }
