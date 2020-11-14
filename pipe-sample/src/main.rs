1104dd5a03e85569ec2ae0ebe1b250c4cbcd378d
moisutsu <moisutsu@gmail.com>
2020-11-14 11:46:23 +0900

    Add execute_command_like_exec

--- pipe-sample/src/main.rs
+++ pipe-sample/src/main.rs
@@ -1,16 +1,17 @@
 use std::process::{Command, Stdio};
 
-fn main() {
-    let mut cmd = Command::new("fd")
-        .arg("python")
-        .arg("/")
+fn main() -> Result<(), std::io::Error> {
+    execute_command_like_exec("python", &["-c", "print('Hello World')"])?;
+    Ok(())
+}
+
+fn execute_command_like_exec(program: &str, args: &[&str]) -> Result<(), std::io::Error> {
+    Command::new(program)
+        .args(args)
+        .stdin(Stdio::inherit())
         .stdout(Stdio::inherit())
         .stderr(Stdio::inherit())
-        .spawn()
-        .unwrap();
-
-    // It's streaming here
-
-    cmd.wait().unwrap();
-    println!("Exited with status {:?}", status);
+        .spawn()?
+        .wait()?;
+    Ok(())
 }
