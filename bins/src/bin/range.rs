4f381ef8aac1f9e2716b84d03be02c01aa67f3be
moisutsu <moisutsu@gmail.com>
2021-07-22 09:38:23 +0900

    Update range

--- bins/src/bin/range.rs
+++ bins/src/bin/range.rs
@@ -1,3 +1,18 @@
+// macro_rules! range {
+//     ($stop:expr) => {
+//         0..$stop
+//     };
+//     ($start:expr, $stop:expr) => {
+//         $start..$stop
+//     };
+//     ($start:expr, $stop:expr, -$step:expr) => {
+//         ($stop + 1..$start + 1).rev().step_by($step)
+//     };
+//     ($start:expr, $stop:expr, $step:expr) => {
+//         ($start..$stop).step_by($step)
+//     };
+// }
+
 macro_rules! range {
     ($stop:expr) => {
         0..$stop
@@ -5,12 +20,22 @@ macro_rules! range {
     ($start:expr, $stop:expr) => {
         $start..$stop
     };
-    ($start:expr, $stop:expr, -$step:expr) => {
-        ($stop + 1..$start + 1).rev().step_by($step)
-    };
-    ($start:expr, $stop:expr, $step:expr) => {
-        ($start..$stop).step_by($step)
-    };
+    ($start:expr, $stop:expr, $step:expr) => {{
+        let step: i64 = $step as i64;
+        #[allow(clippy::reversed_empty_ranges)]
+        if $step < 0 {
+            ($stop + 1..$start + 1)
+                .rev()
+                .step_by(-step as usize)
+                .collect::<Vec<_>>()
+                .into_iter()
+        } else {
+            ($start..$stop)
+                .step_by(step as usize)
+                .collect::<Vec<_>>()
+                .into_iter()
+        }
+    }};
 }
 
 fn main() {
@@ -25,5 +50,9 @@ fn main() {
     }
     for i in range!(3, 1, -1) {
         println!("D: {}", i);
+    }
+    let x = -5;
+    for i in range!(20, 2, x) {
+        println!("E: {}", i);
     }
 }
